a66171282343460afbdf6fbbe894b9aeec41a845
Jose Veliz <jveliz3821@gmail.com>
2019-07-29 19:00:30 -0400

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about' #or can do: get 'pages/about'
-
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
 
 end
