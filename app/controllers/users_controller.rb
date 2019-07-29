9d105580ed458b3d9d132f5aa72d239f83c1ca83
Jose Veliz <jveliz3821@gmail.com>
2019-07-29 19:25:58 -0400

    add user edit and partial for new and edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,34 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "welcome to the Alpha Blog #{@user.username}"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = set_user
+  end
+
+  def update
+    @user = set_user
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
